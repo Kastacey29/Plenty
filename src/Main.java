c54f1b339a06be219e51832343f4a480590c53ae
Stacey29 <Kastacey@yandex.ru>
2022-10-20 21:24:53 +0300

    task done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random=new Random();
+        Set<Integer> plenty=new HashSet<>(20);
+        for (int i = 0; i <20 ; i++) {
+            plenty.add(random.nextInt(1000));
+        }
+        plenty.removeIf(integer -> integer%2==0);
+        System.out.println(plenty);
     }
+
 }
